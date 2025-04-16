e17f7fa5457e0e82189495bb312e56088ec150fe
weslayer <wesleywong1245@yahoo.com>
2025-04-16 14:44:34 -0700

    fix up

--- src/App.java
+++ src/App.java
@@ -1,16 +1,8 @@
 import mvc.*;
 import plague.*;
-import randomWalk.*;
-import simStation.*;
-<<<<<<< HEAD
-import simStation.plague.*;
-=======
->>>>>>> dfd7a77 (put packages outside of simstation)
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // Test, should work:
-        // AppPanel panel = new WorldPanel(new RandomWalkFactory());
         AppPanel panel = new PlaguePanel(new PlagueFactory());
         panel.display();
     }
