8f9cf5fbf67dce128183ccca19ac1fb2aca25a1d
weslayer <wesleywong1245@yahoo.com>
2025-04-16 14:56:58 -0700

    remove app

--- src/App.java
+++
@@ -1,9 +0,0 @@
-import mvc.*;
-import plague.*;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        AppPanel panel = new PlaguePanel(new PlagueFactory());
-        panel.display();
-    }
-}
